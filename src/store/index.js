973e369df6190bd8411f2db776e9f8f0f7f06806
devalzala <devalsinh1210@gmail.com>
2023-08-31 22:35:20 +0530

    changes

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import { proxy } from "valtio";
+import Logo from "../../public/arcreactor.png"
+import texture from "../../public/arcreactor.png"
 
 const state = proxy({
   intro: true,
   color: "#EFBD48",
   isLogoTexture: true,
   isFullTexture: false,
-  logoDecal: "../../public/arcreactor.png",
-  fullDecal: "../../public/Shirttexture.jpg",
+  logoDecal: Logo,
+  fullDecal: texture,
 });
 
 export default state;
